07d69ad62266eb87aa5878924560307e91ec0fed
hemus2121 <hemus2121@gmail.com>
2022-08-19 17:09:12 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (82.69%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -3,10 +3,12 @@ class Solution {
         int n = nums.length;
         
         if (n==1) return nums[0];
-        if (n==2) return Math.max(nums[0], nums[1]);
+        int maxValue = Math.max(nums[0],nums[1]);
+        if (n==2) return maxValue;
+        
         int [] dp = new int [n];
         dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
+        dp[1] = maxValue;
         
         for (int i=2;i< n;i++){
             dp[i] = Math.max(dp[i-1], nums[i]+dp[i-2]);
