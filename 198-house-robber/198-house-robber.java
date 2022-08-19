8020e1dcb605e36dbf605b95e62dc5ca0ba16ad1
hemus2121 <hemus2121@gmail.com>
2022-08-19 17:38:07 +0530

    Time: 0 ms (100.00%), Space: 40 MB (85.59%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,4 +1,29 @@
 class Solution {
+    
+    public int rob (int [] nums){
+        int n = nums.length;
+        int [] dp = new int [n];
+        //if (n==1) return nums[0];
+        //int maxValue = Math.max (nums[0], nums[1]);
+        //if (n==2) return maxValue;
+        Arrays.fill(dp, -1);
+        return compute(nums,n, dp, n-1); 
+    }
+    
+    int compute (int [] nums, int n, int [] dp, int start){
+        
+        //base cases
+        if (start == 0) return Math.max(0, nums[0]);
+        if (start == 1) return Math.max(nums[0],nums[1]);
+        
+        if (dp[start] != -1) return dp[start];
+        
+        int ans =  Math.max(nums[start]+ compute(nums,n, dp,start-2),
+                          compute(nums,n, dp,start-1)) ;
+        return dp[start] = ans;
+    }
+    
+    /* Iterative approach
     public int rob(int[] nums) {
         int n = nums.length;
         
@@ -14,6 +39,5 @@ class Solution {
             dp[i] = Math.max(dp[i-1], nums[i]+dp[i-2]);
         }
         return dp[n-1];
-        
-    }
+    } */
 }
