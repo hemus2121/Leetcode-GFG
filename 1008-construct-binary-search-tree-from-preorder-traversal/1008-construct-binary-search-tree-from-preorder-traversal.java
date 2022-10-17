fcd89d507489dd65441447b0bbb80b67eb192c78
hemus2121 <hemus2121@gmail.com>
2022-10-17 12:11:22 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (92.77%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,16 +14,32 @@
  * }
  */
 class Solution {
+    int index=0;
     public TreeNode bstFromPreorder(int[] preorder) {
-        return consPreOrder(preorder, Integer.MAX_VALUE, new int[]{0});
+        return consPreOrder(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
     
-    TreeNode  consPreOrder(int [] preorder, int bound,  int []index){
+    /* TreeNode  consPreOrder(int [] preorder, int bound,  int []index){
         if (index[0] == preorder.length || preorder[index[0]]> bound ) return null;
         
         TreeNode root = new TreeNode(preorder[index[0]++]);
         root.left = consPreOrder(preorder,root.val, index );
         root.right = consPreOrder(preorder, bound, index);
+        return root;
+    } */
+    
+    TreeNode consPreOrder(int[] preorder, int low, int upper){
+        // base case
+        if (index == preorder.length) return null ;
+        int val = preorder[index];
+        
+        if (val > upper || val < low) return null;
+         // place the current element
+        // and recursively construct subtrees
+        index++;
+        TreeNode root = new TreeNode(val);
+        root.left = consPreOrder(preorder, low, val);
+        root.right = consPreOrder(preorder, val, upper);
         return root;
     }
 }
