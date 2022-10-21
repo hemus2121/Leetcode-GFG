f04be20edac405df9a562c235a145a5cc1305f5b
hemus2121 <hemus2121@gmail.com>
2022-10-21 17:20:51 +0530

    Time: 44 ms (82.36%), Space: 59 MB (94.02%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        
+        Deque <Integer> dq = new LinkedList<>();
+        List<Integer> resList = new ArrayList<>();
+
+        // slide for 1st window of size B
+        for (int i =0;i< k;i++){
+            // keep of removing smaller elements
+            while ( !dq.isEmpty() && dq.getLast()< nums[i]){
+                dq.removeLast();
+            }
+            dq.addLast(nums[i]);
+        }
+        // push the result from front of Q to ans
+        resList.add(dq.getFirst());
+
+        for (int i = k;i< nums.length;i++){
+            while ( !dq.isEmpty() && dq.getLast()< nums[i]){
+                dq.removeLast();
+            }
+            dq.addLast(nums[i]);
+
+            //   remove if outgoing element (i-k) element is same as 1st element in deque
+            if (dq.getFirst()==nums[i-k] ){
+                dq.removeFirst();
+            }
+            resList.add(dq.getFirst());
+        }
+        return resList.stream().mapToInt(val->val).toArray();
+        
+    }
+}
