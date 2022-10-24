74c8e4a2f83ad9e3c20be1d6b5155488745ce7d4
hemus2121 <hemus2121@gmail.com>
2022-10-24 13:10:53 +0530

    Time: 3 ms (7.27%), Space: 43.8 MB (17.78%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low =0, high = nums.length-1;
+        while (low <= high){
+            int mid = (low +high)/2;
+            if (nums[mid]== target) return mid;
+            else if (nums[mid] < target) low = low+1;
+            else high = high-1;
+        }
+        return low;
+        
+    }
+}
