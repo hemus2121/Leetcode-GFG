876e07d2a0a52b054a8a1411596f3fa961ff4181
hemus2121 <hemus2121@gmail.com>
2022-12-28 18:06:01 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (97.03%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,34 +1,23 @@
 class Solution {
     public int maxProduct(int[] nums) {
         
-        int maxLeft = nums[0];
-        int maxRight = nums[0];
-        
-        // traversal from LEFT traversal
-        boolean zeroPresent = false;
+        int max = Integer.MIN_VALUE;
         int prod =1;
+        
+        // traversal from LEFT traversa
         for (int i :nums){
             prod = prod * i;
-            if (i==0){ // resetting it incoming element is ZERO
-                zeroPresent = true;
-                prod =1;
-                continue;
-            }
-            maxLeft = Math.max(maxLeft, prod);
+            max = Math.max(max, prod);
+            if (prod==0) prod =1; // resetting it incoming element is ZERO
         }
         
         //reset prod = 1 for RIGHT traversal
         prod = 1;
-        for (int i = nums.length-1;i >=0;i--){
+        for (int i = nums.length-1;i>=0;i--){
             prod = prod * nums[i];
-            if (nums[i]==0){ // resetting it incoming element is ZERO
-                zeroPresent = true;
-                prod=1;
-                continue;
-            }
-            maxRight = Math.max(maxRight, prod);
+            max = Math.max(max,prod);
+            if (prod==0) prod =1;
         }
-        
-        return zeroPresent ? Math.max(0, Math.max(maxLeft, maxRight)) : Math.max(maxLeft, maxRight);
+        return max;
     }
 }
