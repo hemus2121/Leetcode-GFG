aa33198ae141ea799e5877c4b0b77f8eff6ad7f1
hemus2121 <hemus2121@gmail.com>
2023-01-08 17:13:56 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.java
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.java
@@ -36,9 +36,11 @@ class Solution {
         int dp[]=new int[n];
         Arrays.fill(dp,-1);
         //return computeTopDown(arr,n-1,dp);
-        return computeBottomUp(arr,n, dp);
+        //return computeBottomUp(arr,n, dp);
+        return computeBottomUpOptimized(arr, n, dp);
     }
     
+    //Top up approach
     int computeTopDown(int [] arr, int ind, int [] dp){
         // base case
         if (ind==0) return arr[0];
@@ -53,6 +55,7 @@ class Solution {
         return dp[ind]=Math.max(pick, notPick);
     }
     
+    //Bottom up approach
     int computeBottomUp(int [] arr, int n , int []dp){
         dp[0]= arr[0];
         for (int ind=1; ind<n;ind++){
@@ -65,5 +68,26 @@ class Solution {
         }
         return dp[n-1];
     }
+    
+    // Bottomup -Optimized
+    int computeBottomUpOptimized(int [] arr, int n , int []dp){
+        
+        int prev=arr[0];
+        int prev2=0, curr;
+    
+        for (int ind=1; ind<n;ind++){
+            int Pick = arr[ind];
+            if (ind >1){
+                Pick += prev2;
+            }
+            int nonPick = 0 + prev;
+            curr = Math.max(Pick, nonPick);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+    
+    
     
 }
