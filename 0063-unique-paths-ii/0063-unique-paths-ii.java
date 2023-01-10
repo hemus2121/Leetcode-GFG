c948774d2b47b94cbe0c8bcb085df9c7efc18746
hemus2121 <hemus2121@gmail.com>
2023-01-10 18:24:38 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (84.43%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -6,7 +6,8 @@ class Solution {
         for (int [] row : dp){
             Arrays.fill(row, -1);
         }
-        return computeTopDown(obstacleGrid, m-1, n-1, dp);
+        //return computeTopDown(obstacleGrid, m-1, n-1, dp);
+        return computeBottomUp(obstacleGrid, m, n, dp);
     }
     
     int computeTopDown(int[][]  obstacleGrid, int row, int col, int [][] dp){
@@ -22,5 +23,31 @@ class Solution {
         int up = computeTopDown(obstacleGrid, row-1, col, dp);
         int left = computeTopDown(obstacleGrid, row, col-1, dp);
         return dp[row][col] = up +left;
+    }
+    
+    int computeBottomUp(int[][]  obstacleGrid, int row, int col, int [][] dp ){
+        
+        if (obstacleGrid [0][0]==1) return 0;
+        
+        for (int i =0;i< row;i++){
+            for (int j =0;j<col;j++){
+                
+                if (i==0 && j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                if (i >=0 && j>=0 && obstacleGrid[i][j]==1){
+                    dp[i][j]=0; 
+                    continue;
+                }
+                
+                int up=0, down=0;
+                if (i >0) up = dp[i-1][j];
+                if (j >0) down = dp[i][j-1];
+                
+                dp[i][j] = up + down;
+            }
+        }
+        return dp [row-1][col-1];
     }
 }
