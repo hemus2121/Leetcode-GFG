1909705c99f767498d872df3a951cff88eb7c1c1
hemus2121 <hemus2121@gmail.com>
2023-01-13 11:40:15 +0530

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])throws IOException
+    {
+        //reading input using BufferedReader class
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+        //reading total testcases
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            //reading number of elements and weight
+            int n = Integer.parseInt(read.readLine());
+            int w = Integer.parseInt(read.readLine());
+            
+            int val[] = new int[n];
+            int wt[] = new int[n];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            
+            //inserting the values
+            for(int i = 0; i < n; i++)
+              val[i] = Integer.parseInt(st[i]);
+             
+            String s[] = read.readLine().trim().split("\\s+"); 
+            
+            //inserting the weigths
+            for(int i = 0; i < n; i++)
+              wt[i] = Integer.parseInt(s[i]);
+              
+            //calling method knapSack() of class Knapsack
+            System.out.println(new Solution().knapSack(w, wt, val, n));
+        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution 
+{ 
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int W, int wt[], int val[], int n) 
+    { 
+         // your code here 
+         int [] [] dp = new int [n][W+1];
+         for (int [] row : dp){
+             Arrays.fill(row, -1);
+         }
+         return computeTopDown(W,wt, val, n-1, dp);
+    } 
+    
+    static int computeTopDown(int W, int [] wt, int [] val, int n, int [][]dp){
+        //base case- index is zer
+        if (n==0){
+            if (wt[0] <=W) return val[0];
+            else return 0;
+        }
+        
+        //check if already found
+        if (dp[n][W] != -1) return dp [n][W];
+        
+        int notTaken = 0 + computeTopDown(W, wt, val, n-1, dp);
+        int taken = Integer.MIN_VALUE;
+        if (wt[n] <=W){
+            taken = val[n] + computeTopDown(W-wt[n], wt, val, n-1, dp);
+        }
+        return dp[n][W] = Math.max(notTaken, taken);
+        
+    }
+}
+
+
