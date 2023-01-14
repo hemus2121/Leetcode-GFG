fbc4f3cc4b5ea342d5e1bdb9578d71739fb43695
hemus2121 <hemus2121@gmail.com>
2023-01-14 17:21:38 +0530

    Time: 18 ms (78.91%), Space: 42.8 MB (58.37%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -4,11 +4,15 @@ class Solution {
         int n = A.length;
         int [][] dp = new int [n][B+1];
         
-        for (int [] row : dp ){
-            Arrays.fill(row, -1);
-        }
-        int ans = computeTopDown(A,n-1,B, dp);
-        return ans == (int)1e9 ? -1: ans;
+       // for (int [] row : dp ){
+        //    Arrays.fill(row, -1);
+        //}
+        
+        //int ans = computeTopDown(A,n-1,B, dp);
+        //return ans == (int)1e9 ? -1: ans;
+        
+        int ans = computeBottomUp(A, n, B);
+        return ans >= (int)1e9 ? -1: ans;
     }
     
     int computeTopDown(int []A, int n, int target, int [][]dp){
@@ -24,11 +28,37 @@ class Solution {
         //not pick up coin - reduce count of coins
         int notTaken = computeTopDown(A, n-1, target, dp);
         
-        int taken = (int)Math.pow(10,9);
+        int taken =(int)1e9;
         if ( A[n]<= target){
             // infinite supply of coins --> count of remains same
             taken =  1 + computeTopDown(A,n,target-A[n], dp);
         }
         return dp[n][target] = Math.min(notTaken, taken);
+    }
+    
+    
+    int computeBottomUp(int []A, int n, int B){
+        int [] [] dp = new int [n][B+1];
+        
+        //base case - 
+        for (int i=0;i<= B;i++){
+            if (i%A[0]==0){
+                dp[0][i] = i/A[0];
+            }else dp[0][i] = (int)(1e9);
+        }
+        
+        for (int i =1;i<n;i++){
+            for (int target=0;target<=B;target++){
+                
+                int notTaken = 0 + dp[i-1][target];
+                
+                int taken = (int) (1e9);
+                if (A[i] <= target){
+                    taken = 1+ dp[i][target-A[i]];
+                }
+                dp[i][target] = Math.min(notTaken,taken);
+            }
+        }
+       return dp[n-1][B];
     }
 }
