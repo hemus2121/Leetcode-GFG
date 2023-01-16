c3c3d04e975a9167d47ccc7cc5442703350bc07e
hemus2121 <hemus2121@gmail.com>
2023-01-16 22:23:51 +0530

    Time: 2 ms (96.95%), Space: 42.1 MB (94.38%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if (s.length() != t.length()) return false;
+        
+        int [] fre = new int [26];
+        for (char c : s.toCharArray()){
+            fre[c-'a']++;
+        }
+        
+        for (char c : t.toCharArray()){
+            fre[c-'a']--;
+            if ( fre[c-'a'] < 0) return false;
+        }
+        
+        //for (int i : fre ){
+          //  if (i!=0) return false;
+        //}
+        return true;
+    }
+}
